6c63846f07e2994179d225bf4bdbf8b6b865d2c3
Pedro Guimaraes <pedro.guima@gmail.com>
2015-01-27 12:09:32 -0500

    Small fix

--- investbuddy.sh
+++ investbuddy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# Get info from investors.com
+
+#######################################
+#                                     #
+#  https://github.com/pedroguima      #
+#                                     #
+#######################################
+
 
 FILES="NASDAQ.txt"
 URL="http://research.investors.com/Services/AutoSuggest.asmx/GetQuoteResults"
 HEADER="Content-Type: application/json; charset=UTF-8"
 WGET="/usr/bin/wget"
-#$WGET "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download" -O NASDAQ.txt 
 
 for file in $FILES; do
 	IFS=$'\n'
@@ -19,7 +24,7 @@ for file in $FILES; do
 		leader=$(echo -n "$output" | grep " is ranked 1st in Group" | grep $ticker | sed 's/\n//g')
 		
 		if [ -n "$leader" ]; then 
-			echo "$ticker is a leader in his group"
+			echo "$ticker"
 		fi
 	done	
 done
